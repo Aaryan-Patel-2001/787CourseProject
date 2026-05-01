37dc5efe28b52fe656f0fafa4d093180a91f6f30
Cody Rivera <cjrivera@wisc.edu>
2026-05-01 15:14:33 -0500

    new skewed experiment
    Signed-off-by: Cody Rivera <cjrivera@wisc.edu>

--- Experiments/SkewedData.py
+++ Experiments/SkewedData.py
@@ -8,10 +8,16 @@ from DP_Misra_Gries.Testing import Test, TestKSweep
 from collections import Counter
 
 def make_skewed_stream(n, k, a):
-    raw = np.random.power(a, n)
-    result = np.minimum((raw * k).astype(int), k)
-    return result
+    n_keys = k
+    
+    raw = np.random.pareto(a, size=n_keys) + 1
+    weights = raw / raw.sum()
+    keys = np.random.randint(0, 2**32, size=n_keys, dtype=np.uint32)
 
+    stream = np.random.choice(keys, size=n, p=weights)
+    return stream
+
+    
 def run_experiments(a):
     stream = make_skewed_stream(1000000, 10000, a)
     counts = Counter(stream)
@@ -20,16 +26,36 @@ def run_experiments(a):
     print("distinct observed elements:", len(counts))
     print("starting Experiment")
 
-    k = 100
+
     epsilons = [0.05, 0.1, 0.2, 0.5, 1.0, 2.0, 5.0, 10.0]
-    delta = 1e-6 
-    trials = 3
+    ks = [500, 1000, 2000]
+    delta = 1/(len(stream)*len(stream))
+    trials = 10
     experiment_name = "PowerLaw" + str(a)
-    Test(stream, k, epsilons, delta, trials=trials, ExperimentName=experiment_name)
+    for k in ks:
+        ####### [ Graphs to expect out of this: 4]
+        # Regular AE 
+        # Top-K MAE 
+        # Recall@k 
+        # Max Error 
+        #######
+        Test(stream, k, epsilons, delta, trials=trials, ExperimentName=experiment_name)
+        return
 
-    ks = [5, 10, 50, 100, 150, 200, 500]
-    epsilon=1.0
-    TestKSweep(stream, ks, epsilon, delta, trials=trials, ExperimentName=experiment_name)
 
-run_experiments(5.0)
-run_experiments(10.0)
+    ks = [100, 250, 500, 1000, 2000, 4000, 6000, 8000, 10000]
+    epsilons=[0.05, 0.1, 0.5, 1.0]
+    for epsilon in epsilons:
+        ####### [ Graphs to expect out of this: 4]
+        # Regular AE 
+        # Top-K MAE 
+        # Recall@k 
+        # Max Error 
+        #######
+        TestKSweep(stream, ks, epsilon, delta, trials=trials, ExperimentName=experiment_name)
+
+np.random.seed(42)
+run_experiments(1.0)
+
+np.random.seed(42)
+run_experiments(4.0)
